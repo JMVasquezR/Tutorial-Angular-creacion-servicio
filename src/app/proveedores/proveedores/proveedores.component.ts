defb952cad3e2f6ba7268e44dfd6c0582fb71db1
Marti <jvasquez@softlabperu.com>
2018-08-21 16:41:48 -0500

    Se creo un servicion de un objeto string

--- src/app/proveedores/proveedores/proveedores.component.ts
+++ src/app/proveedores/proveedores/proveedores.component.ts
@@ -9,13 +9,15 @@ import {ProveedoresService} from '../../servicios/proveedores.service';
 })
 export class ProveedoresComponent implements OnInit {
 
-  mensaje: string;
+  // mensaje: string;
+  proveedores: any;
 
   constructor(private proveedoresService: ProveedoresService) {
   }
 
   ngOnInit() {
-    this.mensaje = this.proveedoresService.getProveedores();
+    // this.mensaje = this.proveedoresService.getProveedores();
+    this.proveedores = this.proveedoresService.getProveedores();
   }
 
 }
