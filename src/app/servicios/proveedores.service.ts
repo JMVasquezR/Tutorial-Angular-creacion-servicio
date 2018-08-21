0384488472ab96021250fa032230ad8f2e7b8feb
Marti <jvasquez@softlabperu.com>
2018-08-21 16:23:23 -0500

    Se creo el proyecto appCompras para el desarrollo de creacion de servicio
    Se creo una carpeta servicio el cuandl contiene el servicio del proveedor
    
    Se creo un componente del proveedor

---
+++ src/app/servicios/proveedores.service.ts
@@ -0,0 +1,14 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProveedoresService {
+
+  constructor() {
+  }
+
+  getProveedores() {
+    return 'Mensaje desde el Servicio';
+  }
+}
