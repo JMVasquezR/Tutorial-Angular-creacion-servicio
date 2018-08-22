9a406088aeb1fc9fc9eb63677aa682dee2ebba7c
Marti <jvasquez@softlabperu.com>
2018-08-22 10:15:24 -0500

    Se dio la funcionalidad para capturar los datos del formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 import {AppComponent} from './app.component';
 import {ProveedoresService} from './servicios/proveedores.service';
@@ -26,6 +27,7 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     RouterModule.forRoot(routes)
   ],
   providers: [ProveedoresService],
